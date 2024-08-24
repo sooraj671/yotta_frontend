3bd38c57c979863e187966479c527fdf76d81d8d
ShoetaBadani <badanishoeta766@gmail.com>
2024-08-24 12:22:14 +0500

    Logo Added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,14 +6,26 @@ const Footer = () => {
     <footer className="footer">
       <div className="footer__content">
         <div className="footer__logo">
-          <img src="../../public/yotta_logo.jpg" alt="Logo" className="footer__logo-image" />
+          <img
+            src={`${process.env.PUBLIC_URL}/yotta_logo.jpg`}
+            alt="Logo"
+            className="footer__logo-image"
+          />
         </div>
         <div className="footer__links">
           <ul>
-            <li><a href="/privacy">Privacy Policy</a></li>
-            <li><a href="/terms">Terms of Service</a></li>
-            <li><a href="/contact">Contact Us</a></li>
-            <li><a href="/about">About Us</a></li>
+            <li>
+              <a href="/privacy">Privacy Policy</a>
+            </li>
+            <li>
+              <a href="/terms">Terms of Service</a>
+            </li>
+            <li>
+              <a href="/contact">Contact Us</a>
+            </li>
+            <li>
+              <a href="/about">About Us</a>
+            </li>
           </ul>
         </div>
         <div className="footer__social">
@@ -29,7 +41,10 @@ const Footer = () => {
         </div>
       </div>
       <div className="footer__bottom">
-        <p>&copy; {new Date().getFullYear()} Your Company Name. All rights reserved.</p>
+        <p>
+          &copy; {new Date().getFullYear()} Your Company Name. All rights
+          reserved.
+        </p>
       </div>
     </footer>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,33 @@
-import React from 'react';
-import '../Header.css'; // Optional: for custom styling
+import React from "react";
+import "../Header.css";
 
 const Header = () => {
   return (
     <header className="header">
       <div className="header__logo">
-        <img src="../../public/yotta_logo.jpg" alt="Logo" className="header__logo-image" />
+        <img
+          src={`${process.env.PUBLIC_URL}/yotta_logo.jpg`}
+          alt="Logo"
+          className="header__logo-image"
+        />
       </div>
       <div className="header__title">
         <h1>Yotta Academy</h1>
       </div>
       <nav className="header__nav">
         <ul>
-          <li><a href="/">Home</a></li>
-          <li><a href="/about">About</a></li>
-          <li><a href="/services">Services</a></li>
-          <li><a href="/contact">Contact</a></li>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/services">Services</a>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
         </ul>
       </nav>
       <div className="header__actions">
