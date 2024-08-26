a94ca81d6e037eb9f8b79bc71e5254afae6f18f9
ShoetaBadani <badanishoeta766@gmail.com>
2024-08-27 03:09:22 +0500

    Updated

--- src/components/RoleSelection.js
+++ src/components/RoleSelection.js
@@ -1,27 +1,29 @@
 import React from 'react';
-
+import '../App.css';
 const RoleSelection = ({ onSelectRole }) => {
   return (
-    <section className="container mt-5">
+    <section className="container mt-5 ">
       <h2>Sign Up</h2>
       <div className="row">
         <div className="col-md-6">
-          <button
-            className="btn btn-primary w-100"
-            onClick={() => onSelectRole('tutor')}
-          >
-            Sign Up as Tutor
-          </button>
+         
+            {" "}
+            <button
+              className="btn btn-primary signup-button"
+              onClick={() => onSelectRole("tutor")}
+            >
+              Sign Up as Tutor
+            </button>
+          </div>
+          <div className="col-md-6">
+            <button
+              className="btn btn-primary signup-button"
+              onClick={() => onSelectRole("parent-student")}
+            >
+              Sign Up as Parent/Student
+            </button>
+          </div>
         </div>
-        <div className="col-md-6">
-          <button
-            className="btn btn-primary w-100"
-            onClick={() => onSelectRole('parent-student')}
-          >
-            Sign Up as Parent/Student
-          </button>
-        </div>
-      </div>
     </section>
   );
 };
